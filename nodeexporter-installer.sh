7fb3aa3aa509906ebf142a9a3ba25b62486fa75d
Lee Jon Chapman <me@leejonchapman.com>
2020-02-02 18:20:40 +0000

    initial

---
+++ nodeexporter-installer.sh
@@ -0,0 +1,8 @@
+useradd -M -r -s /bin/false node_exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-386.tar.gz
+cp node_exporter-0.18.1.linux-amd64/node_exporter /usr/local/bin/
+chown node_exporter:node_exporter /usr/local/bin/node_exporter
+cp node_exporter.service /etc/systemd/system/node_exporter.service
+systemctl daemon-reload
+systemctl enable --now node_exporter.service
+systemctl start node_exporter.service
